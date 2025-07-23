604814b3522ed6aebe6323eb835c6b7e9483993b
jmdejesus0807@gmail.com <jpdejesus1@student.hau.edu.ph>
2025-07-24 00:26:58 +0800

    Middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,18 @@ import express from "express";
 import router from "./routers/noteRouter.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js";
 
 
 dotenv.config()
 const app = express()
-connectDB()
 
 app.use(express.json())
+app.use(rateLimiter)
+
 app.use("/api/notes", router)
 
-app.listen(process.env.PORT, () => {
+connectDB().then( () =>{
+    app.listen(process.env.PORT, () => {
     console.log("Your port is", process.env.PORT)
-})
+})})

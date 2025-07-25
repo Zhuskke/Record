67bd59a3d115f2fc23c6757d04ceab3ac887d43d
jmdejesus0807@gmail.com <jpdejesus1@student.hau.edu.ph>
2025-07-26 01:44:02 +0800

    HomePage

--- frontend/src/page/homePage.jsx
+++ frontend/src/page/homePage.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import NavBar from "../components/navBar";
 import RateLimitedUI from "../components/rateLimitedUI";
+import NoteCard from "../components/noteCard";
 
 const homePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -28,10 +29,23 @@ const homePage = () => {
     fetchNote()
   }, [])
   return (
-    <div>
+    <div className="min-h-screen">
       <NavBar />
-
       {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md: grid-cols-2 lg: grid-cols-3 gap-6"> 
+            {notes.map(note => (
+              <div>
+                <NoteCard key={note._id} note={note}/>
+              </div>
+            ))}
+          </div>
+        )}
+
+      </div>
     </div>
   );
 };
